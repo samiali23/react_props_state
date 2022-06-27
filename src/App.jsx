8676a6551a0cc5071938d910d9857cf295e8b9ad
Ben Bryant <brbryant2639@gmail.com>
2022-06-27 06:50:15 -0500

    UPDATE: added box generation for students

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
+    
+    const BOX_COUNT = 24;
+    const BOXES = new Array(BOX_COUNT);
+    
+    for (let i = 0; i < BOXES.length; i++) {
+     BOXES[i] = {
+       id: i + 1,
+       color: `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+      }
+    }
 
     // set default state
 
